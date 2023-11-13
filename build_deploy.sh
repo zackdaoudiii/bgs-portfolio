ada9a737ee3e3cc2ff518ba9ca7de8d1a4ad240d
zakaria daoudi <mr.zakariadaoudi@gmail.com>
2023-11-13 19:27:43 +0100

    change script bash

--- build_deploy.sh
+++ build_deploy.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
+
 # Specify the Docker container name
-CONTAINER_NAME= "bgs-portfolio"
-PORT_ID= 80
+CONTAINER_NAME="bgs-portfolio"
 
 echo "Stopping and removing existing container..."
 docker stop $CONTAINER_NAME
 docker rm $CONTAINER_NAME
 
 echo "re-Building Docker image..."
-docker build -t bgsfront  .
-echo "Build Done ..."
-docker run -d -p $PORT_ID:80
+docker build -t $CONTAINER_NAME  .
 
-echo "project is exposed on "+$PORT_ID
+echo "Build Done ..."
+docker run -d -p 80:80 $CONTAINER_NAME
+
+echo "project is exposed on 80"
